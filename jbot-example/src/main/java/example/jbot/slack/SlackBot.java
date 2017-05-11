aaf598f69f64705209764ff019565459b11be6be
noah.edelstein <noah.edelstein@accenture.com>
2017-05-11 13:44:24 -0500

    Updated spoojnacular client

--- jbot-example/src/main/java/example/jbot/slack/SlackBot.java
+++ jbot-example/src/main/java/example/jbot/slack/SlackBot.java
@@ -1,22 +1,26 @@
+
 package example.jbot.slack;
 
-import me.ramswaroop.jbot.core.slack.Bot;
-import me.ramswaroop.jbot.core.slack.Controller;
-import me.ramswaroop.jbot.core.slack.EventType;
-import me.ramswaroop.jbot.core.slack.models.Event;
-import me.ramswaroop.jbot.core.slack.models.Message;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
-import example.jbot.watson.*;
 
-import java.util.regex.Matcher;
+import example.jbot.spoonacular.Result;
+import example.jbot.spoonacular.SpoonacularRequestUtil;
+import example.jbot.watson.ConversationClient;
+import me.ramswaroop.jbot.core.slack.Bot;
+import me.ramswaroop.jbot.core.slack.Controller;
+import me.ramswaroop.jbot.core.slack.EventType;
+import me.ramswaroop.jbot.core.slack.models.Event;
+import me.ramswaroop.jbot.core.slack.models.Message;
 
 /**
- * A Slack Bot sample. You can create multiple bots by just
- * extending {@link Bot} class like this one.
+ * A Slack Bot sample. You can create multiple bots by just extending {@link Bot} class like this
+ * one.
  *
  * @author ramswaroop
  * @version 1.0.0, 05/06/2016
@@ -24,135 +28,152 @@ import java.util.regex.Matcher;
 @Component
 public class SlackBot extends Bot {
 
-    private static final Logger logger = LoggerFactory.getLogger(SlackBot.class);
-    private ConversationClient client = new ConversationClient();
+	private static final Logger logger = LoggerFactory.getLogger(SlackBot.class);
+	private ConversationClient client = new ConversationClient();
 
-    /**
-     * Slack token from application.properties file. You can get your slack token
-     * next <a href="https://my.slack.com/services/new/bot">creating a new bot</a>.
-     */
-    @Value("${slackBotToken}")
-    private String slackToken;
+	/**
+	 * Slack token from application.properties file. You can get your slack token next
+	 * <a href="https://my.slack.com/services/new/bot">creating a new bot</a>.
+	 */
+	@Value("${slackBotToken}")
+	private String slackToken;
 
-    @Override
-    public String getSlackToken() {
-        return slackToken;
-    }
+	@Override
+	public String getSlackToken() {
+		return slackToken;
+	}
 
-    @Override
-    public Bot getSlackBot() {
-        return this;
-    }
+	@Override
+	public Bot getSlackBot() {
+		return this;
+	}
 
-    /**
-     * Invoked when the bot receives a direct mention (@botname: message)
-     * or a direct message. NOTE: These two event types are added by jbot
-     * to make your task easier, Slack doesn't have any direct way to
-     * determine these type of events.
-     *
-     * @param session
-     * @param event
-     */
-    @Controller(events = {EventType.CHANNEL_JOINED, EventType.HELLO, EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE}, next="sendMessage")
-    public void onReceiveDM(WebSocketSession session, Event event) {
-    	startConversation(event, "sendMessage");
-        reply(session, event, new Message("Welcome to the Diabetics Education Chat. I am DEdA, your Diabetics Education Assistant. I can help you find foods and recipes that meet your dietary needs.  If at any point you want to start over, let me know.Would you like help with food options or recipes?"));
-    }
-    
-    /**
-     * method to send message to ConversationService
-     */
-    @Controller
-    public void sendMessage(WebSocketSession session, Event event) {
-    	String message = event.getText();
-    	String response = client.sendMessage(message);
-    	reply(session, event, new Message(response));
-    }
-    
-    /**
-     * Invoked when bot receives an event of type message with text satisfying
-     * the pattern {@code ([a-z ]{2})(\d+)([a-z ]{2})}. For example,
-     * messages like "ab12xy" or "ab2bc" etc will invoke this method.
-     *
-     * @param session
-     * @param event
-     */
-/*    @Controller(events = EventType.MESSAGE, pattern = "^([a-z ]{2})(\\d+)([a-z ]{2})$")
-    public void onReceiveMessage(WebSocketSession session, Event event, Matcher matcher) {
-        reply(session, event, new Message("First group: " + matcher.group(0) + "\n" +
-                "Second group: " + matcher.group(1) + "\n" +
-                "Third group: " + matcher.group(2) + "\n" +
-                "Fourth group: " + matcher.group(3)));
-    }*/
+	/**
+	 * Invoked when the bot receives a direct mention (@botname: message) or a direct message. NOTE:
+	 * These two event types are added by jbot to make your task easier, Slack doesn't have any
+	 * direct way to determine these type of events.
+	 *
+	 * @param session
+	 * @param event
+	 */
+/*	@Controller(events = { EventType.CHANNEL_JOINED, EventType.HELLO, EventType.DIRECT_MENTION,
+			EventType.DIRECT_MESSAGE }, next = "sendMessage")
+	public void onReceiveDM(WebSocketSession session, Event event) {
+		startConversation(event, "sendMessage");
+		reply(session, event, new Message(
+				"Welcome to the Diabetics Education Chat. I am DEdA, your Diabetics Education Assistant. I can help you find foods and recipes that meet your dietary needs.  If at any point you want to start over, let me know.Would you like help with food options or recipes?"));
+	}*/
 
-
-
-    /**
-     * Conversation feature of JBot. This method is the starting point of the conversation (as it
-     * calls {@link Bot#startConversation(Event, String)} within it. You can chain methods which will be invoked
-     * one after the other leading to a conversation. You can chain methods with {@link Controller#next()} by
-     * specifying the method name to chain with.
-     *
-     * @param session
-     * @param event
-     */
- /*   @Controller(pattern = "(setup meeting)", next = "confirmTiming")
-    public void setupMeeting(WebSocketSession session, Event event) {
-        startConversation(event, "confirmTiming");   // start conversation
-        reply(session, event, new Message("Cool! At what time (ex. 15:30) do you want me to set up the meeting?"));
-    }*/
-
-    /**
-     * This method is chained with {@link SlackBot#setupMeeting(WebSocketSession, Event)}.
-     *
-     * @param session
-     * @param event
-     */
-/*    @Controller(next = "askTimeForMeeting")
-    public void confirmTiming(WebSocketSession session, Event event) {
-        reply(session, event, new Message("Your meeting is set at " + event.getText() +
-                ". Would you like to repeat it tomorrow?"));
-        nextConversation(event);    // jump to next question in conversation
-    }*/
-
-    /**
-     * This method is chained with {@link SlackBot#confirmTiming(WebSocketSession, Event)}.
-     *
-     * @param session
-     * @param event
-     */
-/*    @Controller(next = "askWhetherToRepeat")
-    public void askTimeForMeeting(WebSocketSession session, Event event) {
-        if (event.getText().contains("yes")) {
-            reply(session, event, new Message("Okay. Would you like me to set a reminder for you?"));
-            nextConversation(event);    // jump to next question in conversation  
-        } else {
-            reply(session, event, new Message("No problem. You can always schedule one with 'setup meeting' command."));
-            stopConversation(event);    // stop conversation only if user says no
-        }
-    }*/
-
-    /**
-     * This method is chained with {@link SlackBot#askTimeForMeeting(WebSocketSession, Event)}.
-     *
-     * @param session
-     * @param event
-     */
-/*    @Controller
-    public void askWhetherToRepeat(WebSocketSession session, Event event) {
-        if (event.getText().contains("yes")) {
-            reply(session, event, new Message("Great! I will remind you tomorrow before the meeting."));
-        } else {
-            reply(session, event, new Message("Oh! my boss is smart enough to remind himself :)"));
-        }
-        stopConversation(event);    // stop conversation
-    }*/
-    
-    
-    @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE}, pattern="(test connection)")
-    public void checkConnection(WebSocketSession session, Event event) {
-    	String message = "hello";
+	/**
+	 * method to send message to ConversationService
+	 */
+	/*@Controller
+	public void sendMessage(WebSocketSession session, Event event) {
+		String message = event.getText();
 		String response = client.sendMessage(message);
 		reply(session, event, new Message(response));
-    }
+	}*/
+
+	/**
+	 * Invoked when bot receives an event of type message with text satisfying the pattern
+	 * {@code ([a-z ]{2})(\d+)([a-z ]{2})}. For example, messages like "ab12xy" or "ab2bc" etc will
+	 * invoke this method.
+	 *
+	 * @param session
+	 * @param event
+	 */
+	/*
+	 * @Controller(events = EventType.MESSAGE, pattern = "^([a-z ]{2})(\\d+)([a-z ]{2})$") public
+	 * void onReceiveMessage(WebSocketSession session, Event event, Matcher matcher) {
+	 * reply(session, event, new Message("First group: " + matcher.group(0) + "\n" +
+	 * "Second group: " + matcher.group(1) + "\n" + "Third group: " + matcher.group(2) + "\n" +
+	 * "Fourth group: " + matcher.group(3))); }
+	 */
+
+	/**
+	 * Conversation feature of JBot. This method is the starting point of the conversation (as it
+	 * calls {@link Bot#startConversation(Event, String)} within it. You can chain methods which
+	 * will be invoked one after the other leading to a conversation. You can chain methods with
+	 * {@link Controller#next()} by specifying the method name to chain with.
+	 *
+	 * @param session
+	 * @param event
+	 */
+	/*
+	 * @Controller(pattern = "(setup meeting)", next = "confirmTiming") public void
+	 * setupMeeting(WebSocketSession session, Event event) { startConversation(event,
+	 * "confirmTiming"); // start conversation reply(session, event, new
+	 * Message("Cool! At what time (ex. 15:30) do you want me to set up the meeting?")); }
+	 */
+
+	/**
+	 * This method is chained with {@link SlackBot#setupMeeting(WebSocketSession, Event)}.
+	 *
+	 * @param session
+	 * @param event
+	 */
+	/*
+	 * @Controller(next = "askTimeForMeeting") public void confirmTiming(WebSocketSession session,
+	 * Event event) { reply(session, event, new Message("Your meeting is set at " + event.getText()
+	 * + ". Would you like to repeat it tomorrow?")); nextConversation(event); // jump to next
+	 * question in conversation }
+	 */
+
+	/**
+	 * This method is chained with {@link SlackBot#confirmTiming(WebSocketSession, Event)}.
+	 *
+	 * @param session
+	 * @param event
+	 */
+	/*
+	 * @Controller(next = "askWhetherToRepeat") public void askTimeForMeeting(WebSocketSession
+	 * session, Event event) { if (event.getText().contains("yes")) { reply(session, event, new
+	 * Message("Okay. Would you like me to set a reminder for you?")); nextConversation(event); //
+	 * jump to next question in conversation } else { reply(session, event, new
+	 * Message("No problem. You can always schedule one with 'setup meeting' command."));
+	 * stopConversation(event); // stop conversation only if user says no } }
+	 */
+
+	/**
+	 * This method is chained with {@link SlackBot#askTimeForMeeting(WebSocketSession, Event)}.
+	 *
+	 * @param session
+	 * @param event
+	 */
+	/*
+	 * @Controller public void askWhetherToRepeat(WebSocketSession session, Event event) { if
+	 * (event.getText().contains("yes")) { reply(session, event, new
+	 * Message("Great! I will remind you tomorrow before the meeting.")); } else { reply(session,
+	 * event, new Message("Oh! my boss is smart enough to remind himself :)")); }
+	 * stopConversation(event); // stop conversation }
+	 */
+
+	@Controller(events = { EventType.DIRECT_MENTION,
+			EventType.DIRECT_MESSAGE }, pattern = "(test connection)")
+	public void checkConnection(WebSocketSession session, Event event) {
+		String message = "hello";
+		String response = client.sendMessage(message);
+		reply(session, event, new Message(response));
+	}
+
+	@Controller(events = { EventType.DIRECT_MENTION,
+			EventType.DIRECT_MESSAGE }, pattern = "(test recipe)")
+	public void checkRecipe(WebSocketSession session, Event event) {
+		List<Result> resultList = SpoonacularRequestUtil.complexRecipeSearch(
+				"American, Italian, Polynesian", "Coconut, Lentil",
+				 "salad", "Lunch");
+		if (resultList == null) {
+			reply(session, event,
+					new Message("No recipes matched your request"));
+		} else {
+			reply(session, event,
+					new Message("Here are some recipes which match your meal parameters:"));
+			int i = 1;
+			for ( Result r : resultList) {
+				reply(session, event, new Message(i + ": " + r.getTitle()));
+			}
+		}
+
+	}
+
 }
--- jbot-example/src/main/java/example/jbot/spoonacular/SpoonacularRequestUtil.java
+++ jbot-example/src/main/java/example/jbot/spoonacular/SpoonacularRequestUtil.java
@@ -3,12 +3,14 @@
  */
 package example.jbot.spoonacular;
 
-import static org.junit.Assert.fail;
-
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +24,12 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * @author noah.edelstein
  *
  */
-public class SpoonacularClientTest {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(SpoonacularClientTest.class);
+public class SpoonacularRequestUtil {
+
+	private SpoonacularRequestUtil() {}
+	private static final String MASHAPEKEY = "aDFavnQoytmshh0XVqB9ujdXmClFp1webq5jsnRXqBhZmQmyiW";
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpoonacularRequestUtil.class);
+	private static final String UTF8 = "UTF-8";
 	
 	static {
 		// Only one time
@@ -50,16 +55,24 @@ public class SpoonacularClientTest {
 		});
 	}
 	
-	
-	@Test
-	public void searchRecipesComplexTest() {
-		
-		 try {
-			HttpResponse<ComplexRecipes> complexRecipeList = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=american&excludeIngredients=coconut%2C+mango&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato&instructionsRequired=true&intolerances=peanut%2C+shellfish&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=burger&ranking=2&type=main+course")
-					.header("X-Mashape-Key", "aDFavnQoytmshh0XVqB9ujdXmClFp1webq5jsnRXqBhZmQmyiW")
+	public static List<Result> complexRecipeSearch(String cuisine, String excludeIngredients,
+			String query, String type) {
+		List<Result> resultList = null;
+		try {
+			String queryString = "addRecipeInformation=true&cuisine=" + URLEncoder.encode(cuisine.trim(), UTF8) + "&excludeIngredients=" + 
+					URLEncoder.encode(excludeIngredients.trim(), UTF8) + 
+					"&fillIngredients=false&instructionsRequired=true&limitLicense=false&maxCalories=1500&maxCarbs=45&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=" + 
+					URLEncoder.encode(query, UTF8) + 
+					"&ranking=2&type=" + URLEncoder.encode(type.trim(), UTF8);		
+			HttpResponse<ComplexRecipes> complexRecipeList = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?" + queryString)
+					.header("X-Mashape-Key", MASHAPEKEY)
 					.header("Accept", "application/json").asObject(ComplexRecipes.class);
 			LOGGER.debug("status: " + complexRecipeList.getStatusText());
 			LOGGER.debug(Integer.valueOf(complexRecipeList.getStatus()).toString());
+			InputStream in = complexRecipeList.getRawBody();
+			Reader reader = new InputStreamReader(in);
+			//reader.
+
 			ComplexRecipes list = complexRecipeList.getBody();
 			List<Result> results = list.getResults();
 			for (Result result : results) {
@@ -67,9 +80,11 @@ public class SpoonacularClientTest {
 			}
 
 		} catch (UnirestException e) {
-			fail(e.getMessage());
+			LOGGER.error(e.getMessage(), e);
+		} catch (UnsupportedEncodingException e) {
+			LOGGER.error(e.getMessage(), e);
 		}
-				
+		return resultList;
 	}
 	
 }
